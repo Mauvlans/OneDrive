2c633a78bcce4cf2a097fc7df81fdea746f1a22d
Adam <40191591+Mauvlans@users.noreply.github.com>
2021-03-25 11:14:06 -0700

    Update CreateTaskToMaptoLocalFolder.ps1

--- CreateTaskToMaptoLocalFolder.ps1
+++ CreateTaskToMaptoLocalFolder.ps1
@@ -1,6 +1,7 @@
 # ***************************************************************************
 #
-# Purpose: This script creates a schedualed task and executes 
+# Purpose: This script creates a schedualed task and executes subst during
+# logon
 #
 # ------------- DISCLAIMER -------------------------------------------------
 # This script code is provided as is with no guarantee or waranty concerning
@@ -14,32 +15,15 @@
 #
 # ***************************************************************************
 
-# Script Added Here
-$content = @'
-
-Start-Process cmd -Argument "/c subset J: %onedrive%\documents"
-
-'@
- 
-# creates custom folder and write PS script
-
-$path = $(Join-Path $env:ProgramData CustomScripts)
-if (!(Test-Path $path))
-{
- New-Item -Path $path -ItemType Directory -Force -Confirm:$false
-}
-Out-File -FilePath $(Join-Path $env:ProgramData CustomScripts\MapJDrive.ps1) -Encoding unicode -Force -InputObject $content -Confirm:$false
- 
 # register script as scheduled task
 
-$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-ExecutionPolicy Bypass -NonInteractive -WindowStyle Hidden -File C:\Programdata\CustomScripts\MapJDrive.ps1"
+$action = New-ScheduledTaskAction -Execute 'cmd.exe' -Argument "/c subst j: ""%ONEDRIVE%\Documents"""
 $trigger =  New-ScheduledTaskTrigger -AtLogon -RandomDelay (New-TimeSpan -Minutes 1)
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden -DontStopIfGoingOnBatteries -Compatibility Win8
 $principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Users"
 $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal
 Register-ScheduledTask -InputObject $task -TaskName "MapJDrive"
 
-#wait 10 Seconds and Kick off Schedualed Task
+#Kick off Schedualed Task
 
-Start-Sleep 10
 Start-ScheduledTask -TaskName "MapJDrive"
